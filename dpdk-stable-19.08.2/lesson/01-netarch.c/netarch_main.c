55f08a3f41bc951fb2d2a36e76da69e33bbe042b
595787398 <595787398@qq.com>
2022-05-23 23:15:23 -0700

    修改编译报错，并调试功能

--- dpdk-stable-19.08.2/lesson/01-netarch.c/netarch_main.c
+++ dpdk-stable-19.08.2/lesson/01-netarch.c/netarch_main.c
@@ -1,4 +1,10 @@
-
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_mbuf.h>
+#include <rte_malloc.h>
+#include <rte_timer.h>
+#include <stdio.h>
+#include <arpa/inet.h>
 
 #define TIMER_RESOLUTION_CYCLES 120000000000ULL // 10ms * 1000 = 10s * 6
 #define MBUF_NUMS	8191
@@ -7,11 +13,11 @@ int g_port_id = 0;
 
 #define IP_STR_COVER_UINT(a, b, c, d) (a + (b << 8) + (c << 16) + (d << 24))
 
-uint32 g_local_src_ip = IP_STR_COVER_UINT(192, 168, 1, 2);
+uint32_t g_local_src_ip = IP_STR_COVER_UINT(192, 168, 1, 2);
 
-char* mac2string_print(uint8_t *mac)
+static char *mac2string_print(uint8_t *mac)
 {
-	static char buf[sizeof("HH:HH:HH:HH:HH:HH")];
+	static char buf[sizeof("HH:HH:HH:HH:HH:HH") + 1];
 
 	snprintf(buf, sizeof(buf) -1 ,"%02x:%02x:%02x:%02x:%02x:%02x",
 		mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
@@ -20,9 +26,9 @@ char* mac2string_print(uint8_t *mac)
 }
 
 /* src-mac | dst-mac | frame-type*/
-int eth_packet_encap(uint8 *packet, uint8 *src_mac, uint8 *dst_mac, uint16_t ether_type)
+static int eth_packet_encap(uint8_t *packet, uint8_t *src_mac, uint8_t *dst_mac, uint16_t ether_type)
 {
-	struct rte_ether_hdr *eth_packet = packet;
+	struct rte_ether_hdr *eth_packet = (struct rte_ether_hdr *)packet;
 
 	rte_memcpy(eth_packet->d_addr.addr_bytes, dst_mac, sizeof(eth_packet->d_addr.addr_bytes)) ,
 
@@ -36,10 +42,10 @@ int eth_packet_encap(uint8 *packet, uint8 *src_mac, uint8 *dst_mac, uint16_t eth
 /*
 	|arp
 */
-int arp_packet_encap(uint8 *packet, uint8_t *src_mac, uint8_t *dst_mac,
+static int arp_packet_encap(uint8_t *packet, uint8_t *src_mac, uint8_t *dst_mac,
 	uint32_t src_ip, uint32_t dst_ip, uint16_t opcode)
 {
-	struct rte_arp_hdr *arp_packet = packet;
+	struct rte_arp_hdr *arp_packet = (struct rte_arp_hdr *)packet;
 
 	arp_packet->arp_hardware = htons(1);
 	arp_packet->arp_protocol = htons(RTE_ETHER_TYPE_IPV4);
@@ -55,10 +61,9 @@ int arp_packet_encap(uint8 *packet, uint8_t *src_mac, uint8_t *dst_mac,
 	return 0;
 }
 
-struct rte_mbuf * arp_reply_packet_encap(struct rte_mempool *mp, int port_id,
+static struct rte_mbuf * arp_reply_packet_encap(struct rte_mempool *mp, int port_id,
 	struct rte_ether_addr *dst_mac, uint32_t src_ip, uint32_t dst_ip)
 {
-	int packet_len;
 	struct rte_ether_addr src_mac;
 
 	struct rte_mbuf *mbuf = rte_pktmbuf_alloc(mp);
@@ -66,13 +71,20 @@ struct rte_mbuf * arp_reply_packet_encap(struct rte_mempool *mp, int port_id,
 		rte_exit(-1, "arp replcy packet malloc failed");
 	}
 
+	mbuf->pkt_len = sizeof(struct rte_ether_hdr) + sizeof(struct rte_arp_hdr);
+	mbuf->data_len = sizeof(struct rte_ether_hdr) + sizeof(struct rte_arp_hdr);
+
 	struct rte_ether_hdr *eth_hdr = rte_pktmbuf_mtod(mbuf, struct rte_ether_hdr *);
 	rte_eth_macaddr_get(port_id, &src_mac);
-	eth_packet_encap(eth_hdr, &src_mac, dst_mac, RTE_ETHER_TYPE_ARP);
+	eth_packet_encap((uint8_t *)eth_hdr, (uint8_t *)&src_mac, (uint8_t *)dst_mac, RTE_ETHER_TYPE_ARP);
 
 	/* arp */
 	struct rte_arp_hdr *arp_hdr = rte_pktmbuf_mtod_offset(mbuf, struct rte_arp_hdr *, sizeof(struct rte_ether_hdr));
-	arp_packet_encap(arp_hdr, &src_mac, dst_mac, src_ip, dst_ip, RTE_ARP_OP_REPLY);
+	arp_packet_encap((uint8_t *)arp_hdr, (uint8_t *)&src_mac, (uint8_t *)dst_mac, src_ip, dst_ip, RTE_ARP_OP_REPLY);
+	printf("send arp reply:[MAC:%s IP:%s]",  mac2string_print(arp_hdr->arp_data.arp_sha.addr_bytes),
+		inet_ntoa(*(struct in_addr *)&arp_hdr->arp_data.arp_sip));
+	printf("send arp reply:[MAC:%s IP:%s]\n", mac2string_print(arp_hdr->arp_data.arp_tha.addr_bytes),
+		inet_ntoa(*(struct in_addr *)&arp_hdr->arp_data.arp_tip));
 
 	return mbuf;
 }
@@ -82,12 +94,12 @@ static void arp_request_timer_cb(__attribute__((unused)) struct rte_timer *tim,
 	   void *arg)
 {
 	int i;
-	uint8 *packet;
+	uint8_t *packet;
 	struct rte_ether_addr mac_addr;
 	struct rte_mempool *mem_pool =  (struct rte_mempool *)arg;
 	uint8_t arp_dst_mac[RTE_ETHER_ADDR_LEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
 
-	rte_eth_macaddr_get(port_id, &mac_addr);
+	rte_eth_macaddr_get(g_port_id, &mac_addr);
 
 	struct rte_mbuf *mbuf = rte_pktmbuf_alloc(mem_pool);
 	if (!mbuf) {
@@ -96,17 +108,17 @@ static void arp_request_timer_cb(__attribute__((unused)) struct rte_timer *tim,
 
 	for (i = 1; i <= 254; i++) {
 
-		packet = rte_pktmbuf_mtod(mbuf, (uint8 *));
+		packet = rte_pktmbuf_mtod(mbuf, uint8_t *);
 
 		/* 构造以太网头 */
-		eth_packet_encap(packet, mac_addr, arp_dst_mac, RTE_ETHER_TYPE_ARP);
+		eth_packet_encap(packet, (uint8_t *)&mac_addr, (uint8_t *)arp_dst_mac, RTE_ETHER_TYPE_ARP);
 
 		/*构造arp报文*/
-		arp_request_packet_encap(packet + sizeof(struct rte_ether_hdr),
-			mac_addr, arp_dst_mac, g_local_src_ip, IP_STR_COVER_UINT(192, 168, 1, i), RTE_ARP_OP_REQUEST);
+		arp_packet_encap(packet + sizeof(struct rte_ether_hdr),
+			(uint8_t *)&mac_addr, (uint8_t *)arp_dst_mac, g_local_src_ip, IP_STR_COVER_UINT(192, 168, 1, i), RTE_ARP_OP_REQUEST);
 
 		/* 发送报文 */
-		rte_eth_tx_burst(port_id, 0, &packet, 1);
+		rte_eth_tx_burst(g_port_id, 0, &packet, 1);
 
 	}
 }
@@ -119,12 +131,12 @@ static const struct rte_eth_conf port_conf_default = {
 
 
 /* 设定网口的收发包队列 */
-static dpdk_eth_dev_init(struct rte_mempool * mp)
+static void dpdk_eth_dev_init(struct rte_mempool * mp)
 {
 	uint16_t ports;
 	struct rte_eth_dev_info dev_info;
 
-	ports = rte_eth_dev_count_avail(void);
+	ports = rte_eth_dev_count_avail();
 	if (ports == 0) {
 		rte_exit(-1, "dev count failed");
 	}
@@ -140,9 +152,9 @@ static dpdk_eth_dev_init(struct rte_mempool * mp)
 	}
 
 	struct rte_eth_txconf txq_conf = dev_info.default_txconf;
-	txq_conf.offloads = port_conf.rxmode.offloads;
+	txq_conf.offloads = port_conf_default.rxmode.offloads;
 
-	if (rte_eth_tx_queue_setup(g_port_id, 0, , 1024, rte_eth_dev_socket_id(g_port_id), &txq_conf) < 0) {
+	if (rte_eth_tx_queue_setup(g_port_id, 0, 1024, rte_eth_dev_socket_id(g_port_id), &txq_conf) < 0) {
 		rte_exit(-1, "tx init failed");
 	}
 
@@ -152,10 +164,11 @@ static dpdk_eth_dev_init(struct rte_mempool * mp)
 
 }
 
-int arp_packet_parse(struct rte_mbuf *rx_packet, struct rte_ether_addr *src_mac,
+static int arp_packet_parse(struct rte_mbuf *rx_packet, struct rte_ether_addr *src_mac,
 		struct rte_ether_addr *dst_mac, uint32_t *src_ip ,uint32_t *dst_ip)
 {
-	struct rte_arp_hdr *arp_hdr = rte_pktmbuf_mtod_offset(rx_packet, sizeof(struct rte_ether_addr));
+	struct rte_arp_hdr *arp_hdr = rte_pktmbuf_mtod_offset(rx_packet, struct rte_arp_hdr *,
+		sizeof(struct rte_ether_hdr));
 
 	rte_memcpy(src_mac, &arp_hdr->arp_data.arp_sha, sizeof(struct rte_ether_addr));
 	rte_memcpy(dst_mac, &arp_hdr->arp_data.arp_tha, sizeof(struct rte_ether_addr));
@@ -208,23 +221,25 @@ int main(int argc, char **argv)
 		for (i = 0; i < recv_nums; i++) {
 			ether_hdr = rte_pktmbuf_mtod(rx_pkts[i], struct rte_ether_hdr *);
 			//if (ether_hdr->ether_type)
-			printf("ether type:%d\n", ether_hdr->ether_type);
+			printf("ether type:%02x\n", ntohs(ether_hdr->ether_type));
 
-			if (ether_hdr->ether_type == RTE_ETHER_TYPE_ARP) {
+			if (ntohs(ether_hdr->ether_type) == RTE_ETHER_TYPE_ARP) {
 				arp_packet_parse(rx_pkts[i], &src_mac, &dst_mac, &src_ip, &dst_ip);
-				printf("arp packet:[%s:%s]--->", mac2string_print(src_mac.addr_bytes), inet_ntoa(src_ip));
-				printf("arp packet:[%s:%s]", mac2string_print(dst_mac.addr_bytes), inet_ntoa(dst_ip));
-				struct rte_arp_hdr *arp_hdr = rte_pktmbuf_mtod_offset(rx_pkts[i], sizeof(struct rte_ether_addr));
+				printf("arp packet:[MAC:%s IP:%s]--->", mac2string_print(src_mac.addr_bytes), inet_ntoa(*(struct in_addr *)&src_ip));
+				printf("arp packet:[MAC:%s IP:%s]\n", mac2string_print(dst_mac.addr_bytes), inet_ntoa(*(struct in_addr *)&dst_ip));
+				struct rte_arp_hdr *arp_hdr = rte_pktmbuf_mtod_offset(rx_pkts[i],
+					struct rte_arp_hdr *, sizeof(struct rte_ether_hdr));
 
-				if (arp_hdr->arp_opcode == RTE_ARP_OP_REQUEST) {
+				if (ntohs(arp_hdr->arp_opcode) == RTE_ARP_OP_REQUEST) {
 					/* 接收arp请求报文，并发送响应报文 */
 
 					struct rte_mbuf * tx_packet = arp_reply_packet_encap(mempool, g_port_id,
-						dst_mac, src_ip, dst_ip);
+						&src_mac, dst_ip, src_ip);
+
 
 					rte_eth_tx_burst(g_port_id, 0, &tx_packet, 1);
 
-				} else if (arp_hdr->arp_opcode == RTE_ARP_OP_REPLY) {
+				} else if (ntohs(arp_hdr->arp_opcode) == RTE_ARP_OP_REPLY) {
 					/* 接收arp响应报文 */
 
 				}
